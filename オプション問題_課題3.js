644f117b3f3cbbe1df7c136ef15de5f6b827020e
Ayako Kurita <107830445+kurigohan0103@users.noreply.github.com>
2023-07-06 16:38:58 +0900

    Add files via upload

---
+++ オプション問題_課題3.js
@@ -0,0 +1,5 @@
+const drink = ["coffee", "milk", "coke", "tea"];
+
+for (let i = 0; i < drink.length; i++) {
+  console.log(drink[i]);
+}
---
+++ オプション問題_課題4.js
@@ -0,0 +1,9 @@
+function sumNumbers() {
+    let sum = 0;
+    for (let i = 1; i <= 100; i++) {
+      sum += i;
+    }
+    return console.log(sum);
+  }
+  
+  sumNumbers();
